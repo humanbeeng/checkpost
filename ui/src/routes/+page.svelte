815884ce9fd88deec94800ae4db777a93725884c
Nithin <thenithinraj@gmail.com>
2024-03-09 01:36:34 +0530

    Modify blur value

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
-<header class="sticky top-0 border-b border-black/10 w-full bg-gray-100/10 backdrop-blur-xl">
+<header class="sticky top-0 border-b border-black/10 w-full bg-gray-100/10 backdrop-blur-md">
 	<div class="container flex h-12 max-w-screen-2xl items-center justify-between">
 		<a href="/" class="font-medium">Checkpost</a>
 		<nav class="flex items-center gap-6 text-sm">
