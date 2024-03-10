e96114ba50e59f61a5502c480704498a964de9ce
Nithin <thenithinraj@gmail.com>
2024-03-10 14:04:38 +0530

    Fix footer padding

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -504,21 +504,20 @@
 			<!-- </Card.Footer> -->
 			<!-- </Card.Root> -->
 		</div>
-	</section>
-
-	<footer class="py-4 md:px-8 md:py-0">
-		<div class="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-			<div class="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-				<p class="text-center text-sm leading-loose text-muted-foreground md:text-left">
-					Built by <a
-						href="https://twitter.com/nithinrajx"
-						target="_blank"
-						rel="noreferrer"
-						class="font-medium underline underline-offset-4"
-						data-svelte-h="svelte-18aekvv">Nithin Raj</a
-					>.
-				</p>
+		<footer class=" md:px-8 md:py-0 bottom-0">
+			<div class="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+				<div class="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+					<p class="text-center text-sm leading-loose text-muted-foreground md:text-left">
+						Built by <a
+							href="https://twitter.com/nithinrajx"
+							target="_blank"
+							rel="noreferrer"
+							class="font-medium underline underline-offset-4"
+							data-svelte-h="svelte-18aekvv">Nithin Raj</a
+						>.
+					</p>
+				</div>
 			</div>
-		</div>
-	</footer>
+		</footer>
+	</section>
 </div>
