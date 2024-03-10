9b0bb75b7476fab3e65a5933068123d1991fb62c
Nithin <thenithinraj@gmail.com>
2024-03-10 14:05:49 +0530

    Fix footer padding

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -504,7 +504,7 @@
 			<!-- </Card.Footer> -->
 			<!-- </Card.Root> -->
 		</div>
-		<footer class=" md:px-8 md:py-0 bottom-0">
+		<footer class="py-4 md:px-8 md:py-0 bottom-0">
 			<div class="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
 				<div class="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
 					<p class="text-center text-sm leading-loose text-muted-foreground md:text-left">
