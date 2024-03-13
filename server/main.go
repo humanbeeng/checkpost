cfa8559a09f50c4c861852ed2e6bb1333ae57be9
Nithin <thenithinraj@gmail.com>
2024-03-13 19:43:22 +0530

    Add test routes

--- server/main.go
+++ server/main.go
@@ -13,25 +13,31 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Get("/about", func(c *fiber.Ctx) error {
-		return c.SendString("about page")
+	app.Get("/v1/auth/google", func(c *fiber.Ctx) error {
+		return c.SendString("Google login")
 	})
 
-	app.All(":path", func(c *fiber.Ctx) error {
+	app.Get("/dashboard", func(c *fiber.Ctx) error {
+		return c.SendString("Dashboard")
+	})
+
+	app.Get("/profile", func(c *fiber.Ctx) error {
+		return c.SendString("Profile")
+	})
+
+	app.All("/v1/endpoint/:path", func(c *fiber.Ctx) error {
 		var req any
 
-		if err := c.BodyParser(&req); err != nil {
-			return fiber.NewError(fiber.ErrBadRequest.Code, fiber.ErrBadRequest.Message)
-		}
+		_ = c.BodyParser(&req)
 
 		str, err := json.Marshal(req)
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		fmt.Println(string(str))
+		msg := fmt.Sprintf("Path: %v \nBody: %v", c.Path(), string(str))
 
-		return c.SendString(c.Params("path"))
+		return c.SendString(msg)
 	})
 
 	app.Listen(":8080")
