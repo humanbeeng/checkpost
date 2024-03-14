b29cad80452c4a685d9d8533ff22ffddd11b8def
Nithin <thenithinraj@gmail.com>
2024-03-14 20:27:10 +0530

    Add url endpoint handler

--- server/.null-ls_966176_main.go
+++ server/.null-ls_966176_main.go
@@ -9,6 +9,14 @@ import (
 
 func main() {
 	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("From: %v", c.Subdomains())
+		return c.SendString(msg)
+	})
+
+	app.Get("/v1/auth/google", func(c *fiber.Ctx) error {
+		return c.SendString("Google login")
+	})
 
 	app.Get("/dashboard", func(c *fiber.Ctx) error {
 		return c.SendString("Dashboard")
@@ -18,12 +26,15 @@ func main() {
 		return c.SendString("Profile")
 	})
 
-	app.All("/url/:path", func(c *fiber.Ctx) error {
+	app.All("/v1/url/:path", func(c *fiber.Ctx) error {
 		var req any
 
 		_ = c.BodyParser(&req)
 
-		str, _ := json.Marshal(req)
+		str, err := json.Marshal(req)
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		msg := fmt.Sprintf("Path: %v \nBody: %v", c.Path(), string(str))
 		fmt.Println(msg)
