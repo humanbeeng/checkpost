766011eb5fa6c8f9b5b1507846296a8ca3487981
Nithin <thenithinraj@gmail.com>
2024-04-04 22:37:42 +0530

    refactor: middlewares as core package

---
+++ server/internal/core/paseto.go
@@ -0,0 +1,65 @@
+package core
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/aead/chacha20poly1305"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+	"github.com/o1egl/paseto"
+)
+
+type PasetoVerifier struct {
+	symmetricKey string
+	paseto       *paseto.V2
+}
+
+func NewPasetoVerifier(symmetricKey string) (*PasetoVerifier, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("Invalid key size. Must be exactly %d chars", chacha20poly1305.KeySize)
+	}
+
+	return &PasetoVerifier{
+		symmetricKey: symmetricKey,
+		paseto:       paseto.NewV2(),
+	}, nil
+}
+
+func (p *PasetoVerifier) CreateToken(username string, userId int64, duration time.Duration) (string, error) {
+	id, err := gonanoid.New()
+	if err != nil {
+		return "", err
+	}
+
+	jt := paseto.JSONToken{
+		Issuer:     "checkpost",
+		Jti:        id,
+		Subject:    strconv.FormatInt(userId, 10),
+		IssuedAt:   time.Now(),
+		Expiration: time.Now().Add(duration),
+	}
+	jt.Set("username", username)
+
+	token, err := p.paseto.Encrypt([]byte(p.symmetricKey), jt, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (p *PasetoVerifier) VerifyToken(token string) (paseto.JSONToken, error) {
+	var jt paseto.JSONToken
+
+	err := p.paseto.Decrypt(token, []byte(p.symmetricKey), &jt, nil)
+	if err != nil {
+		return jt, err
+	}
+
+	if time.Now().After(jt.Expiration) {
+		return jt, fmt.Errorf("Token has expired")
+	}
+
+	return jt, nil
+}
