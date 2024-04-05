4362499dafd3521daf215b0d82dd5313a5cc1a0f
Nithin <thenithinraj@gmail.com>
2024-04-05 17:51:35 +0530

    feat: requestid logger middleware

--- server/main.go
+++ server/main.go
@@ -9,6 +9,8 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -34,6 +36,13 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(requestid.New())
+	// TODO: Revisit this configuration and slog configuration
+	app.Use(logger.New(logger.Config{
+		// For more options, see the Config section
+		TimeFormat: "2006/03/01 15:04:05",
+		Format:     "${time} | ${locals:requestid} | ${status} | ${latency} | ${method} ${path}​\n",
+	}))
 
 	key := os.Getenv("PASETO_KEY")
 	pv, err := core.NewPasetoVerifier(key)
@@ -55,7 +64,7 @@ func main() {
 	defer conn.Close()
 	slog.Info("Connection established", "conn", connectionString)
 
-	runDBMigration("file://db/migration", connectionString)
+	// runDBMigration("file://db/migration", connectionString)
 
 	queries := db.New(conn)
 
@@ -63,13 +72,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to init auth controller. %v", err)
 	}
-	ac.RegisterRoutes(app)
 
 	adc := admin.NewAdminController()
-	adc.RegisterRoutes(app, &pmw)
-
 	endpointService := url.NewUrlService(queries)
 	urlHandler := url.NewEndpointHandler(endpointService)
+
+	adc.RegisterRoutes(app, &pmw)
+	ac.RegisterRoutes(app)
 	urlHandler.RegisterRoutes(app, &tmw)
 
 	// TODO: Fetch port from config
