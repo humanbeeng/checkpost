8b88282d8e217aa14f88446658b4d641cb097356
Nithin <thenithinraj@gmail.com>
2024-04-07 12:10:17 +0530

    feat: add plan based rate limiter for hook

---
+++ server/internal/core/middleware/limiter.go
@@ -0,0 +1,81 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	db "github.com/humanbeeng/checkpost/server/db/sqlc"
+)
+
+func NewGuestPlanLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Locals("plan").(string) != string(db.PlanGuest)
+		},
+		Max:               2,
+		Expiration:        time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	})
+}
+
+func NewFreePlanLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Locals("plan").(string) != string(db.PlanFree)
+		},
+		Max:               5,
+		Expiration:        time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Locals("username").(string)
+		},
+	})
+}
+
+func NewNoBrainerPlanLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Locals("plan").(string) != string(db.PlanFree)
+		},
+		Max:               10,
+		Expiration:        time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Locals("username").(string)
+		},
+	})
+}
+
+func NewProPlanLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Locals("plan").(string) != string(db.PlanFree)
+		},
+		Max:               20,
+		Expiration:        time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Locals("username").(string)
+		},
+	})
+}
+
+func NewGenerateUrlLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Max:               1,
+		Expiration:        time.Minute,
+		LimiterMiddleware: limiter.SlidingWindow{},
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return c.Locals("username").(string)
+		},
+	})
+}
+
+func NewGenerateRandomUrlLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Max:               1,
+		Expiration:        time.Minute,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	})
+}
