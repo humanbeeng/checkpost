c7b74b729e1ea869bdebfebc046647ed436f9eec
Nithin <thenithinraj@gmail.com>
2024-04-07 12:07:18 +0530

    style: format endpoint.sql

--- server/db/query/endpoint.sql
+++ server/db/query/endpoint.sql
@@ -1,3 +1,6 @@
+-- name: GetEndpoint :one
+select * from "endpoint" where endpoint = $1 limit 1;
+
 -- name: CreateNewEndpoint :one
 insert into
   endpoint (endpoint, user_id, plan, expires_at)
@@ -10,5 +13,3 @@ insert into endpoint (endpoint, expires_at) values ($1, $2) returning *;
 -- name: CheckEndpointExists :one
 select exists(select * from endpoint where endpoint = $1 limit 1);
 
--- name: GetEndpoint :one
-select * from "endpoint" where endpoint = $1 limit 1;
