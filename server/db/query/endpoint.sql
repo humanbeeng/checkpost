69a3f837ce2eb31a2f4b878e81e1631d39679116
Nithin <thenithinraj@gmail.com>
2024-04-09 22:59:58 +0530

    style: format endpoint.sql

--- server/db/query/endpoint.sql
+++ server/db/query/endpoint.sql
@@ -1,25 +1,49 @@
 -- name: GetEndpoint :one
-select * from "endpoint" where endpoint = $1 limit 1;
+select *
+from "endpoint"
+where
+    endpoint = $1
+    and is_deleted = false
+limit 1;
+
+-- name: CheckEndpointExists :one
+select exists (
+        select *
+        from endpoint
+        where
+            endpoint = $1
+            and expires_at > now()
+            and is_deleted = false
+        limit 1
+    );
+
+-- name: GetNonExpiredEndpointsOfUser :many
+select *
+from "endpoint"
+where
+    user_id = $1
+    and expires_at > now()
+    and is_deleted = false;
 
 -- name: CreateNewEndpoint :one
 insert into
     endpoint (
         endpoint, user_id, plan, expires_at
     )
-values ($1, $2, $3, $4) returning *;
+values ($1, $2, $3, $4)
+returning
+    *;
 
 -- name: CreateNewGuestEndpoint :one
 insert into
     endpoint (endpoint, expires_at)
-values ($1, $2) returning *;
+values ($1, $2)
+returning
+    *;
 
 -- name: CreateNewFreeUrl :one
 insert into
     endpoint (endpoint, user_id, expires_at)
-values ($1, $2, $3) returning *;
-
--- name: CheckEndpointExists :one
-select exists ( select * from endpoint where endpoint = $1 limit 1 );
-
--- name: GetNonExpiredEndpointsOfUser :many
-select * from "endpoint" where user_id = $1 and expires_at > now();
+values ($1, $2, $3)
+returning
+    *;
