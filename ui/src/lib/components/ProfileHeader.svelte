a88d901ccc4af08718c3700a5f7f2b93172e8419
Nithin <thenithinraj@gmail.com>
2024-04-28 01:14:59 +0530

    feat: display reserved subdomain in waitlist

--- ui/src/lib/components/ProfileHeader.svelte
+++ ui/src/lib/components/ProfileHeader.svelte
@@ -4,17 +4,17 @@
 	import { Button } from '@/components/ui/button';
 	import type { User } from '@/types';
 
-	export let user: User | null;
+	export let user: User;
 </script>
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger asChild let:builder>
 		<Button variant="ghost" builders={[builder]} class="relative rounded-md hover:bg-gray-500/10">
 			<Avatar.Root class="h-8 w-8">
-				<Avatar.Image src={user?.avatar_url} alt={user?.name} />
+				<Avatar.Image src={user.avatar_url} alt={user.name} />
 				<!-- <Avatar.Fallback></Avatar.Fallback> -->
 			</Avatar.Root>
-			<p class="px-2" autocapitalize="on">{user?.name}</p>
+			<p class="px-2" autocapitalize="on">{user.name}</p>
 		</Button>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content class="w-56" align="end">
--- ui/src/routes/waitlist/+page.server.ts
+++ ui/src/routes/waitlist/+page.server.ts
@@ -1,6 +1,7 @@
 import { PUBLIC_SERVER_URL } from '$env/static/public';
 import type { User } from '@/types.js';
 import { error, redirect } from '@sveltejs/kit';
+import type { UserEndpointsResponse } from '../onboarding/types.js';
 
 export const load = async ({ fetch, cookies }) => {
 	const token = cookies.get('token');
@@ -8,22 +9,44 @@ export const load = async ({ fetch, cookies }) => {
 		return redirect(301, '/');
 	}
 
-	const res = await fetch(`${PUBLIC_SERVER_URL}/user`).catch((err) => {
-		console.log('Unable to fetch user details', err);
-		return error(500);
-	});
+	const fetchUser = async () => {
+		const res = await fetch(`${PUBLIC_SERVER_URL}/user`).catch((err) => {
+			console.error('Unable to fetch user details', err);
+			return error(500);
+		});
+		if (!res.ok) {
+			// TODO: Better error handling
+			return error(res.status, await res.text());
+		}
 
-	if (!res.ok) {
-		// TODO: Better error handling
-		return {
-			err: { message: 'Unable to fetch user details' }
-		};
-	}
+		const user = (await res.json().catch((err) => {
+			console.error('Unable to parse user response', err);
+		})) as User;
 
-	const user = (await res.json().catch((err) => {
-		console.error('Unable to parse user response', err);
-	})) as User;
+		return user;
+	};
 
-	console.log('User fetched from server', user);
-	return { user };
+	const fetchUrls = async () => {
+		const res = await fetch(`${PUBLIC_SERVER_URL}/url`).catch((err) => {
+			console.log('Unable to fetch user details', err);
+			return error(500);
+		});
+
+		if (!res.ok) {
+			// TODO: Better error handling
+			return error(res.status, await res.text());
+		}
+
+		const urls = (await res.json().catch((err) => {
+			console.error('Unable to parse user response', err);
+		})) as UserEndpointsResponse;
+
+		if (!urls.endpoints || !urls.endpoints.length) {
+			return redirect(301, '/onboarding');
+		}
+
+		return urls;
+	};
+
+	return { user: await fetchUser(), url: await fetchUrls(), err: null };
 };
