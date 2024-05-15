7fe112c7e2a3a3fc7e6d973c130bea193d85a6cf
Nithin <thenithinraj@gmail.com>
2024-05-15 19:30:48 +0530

    feat: remove pricing section

--- ui/src/lib/components/Header.svelte
+++ ui/src/lib/components/Header.svelte
@@ -6,7 +6,7 @@
 	export let user: User | null;
 </script>
 
-<header class=" sticky top-0 border-b border-black/10 w-full bg-gray-100/10 backdrop-blur-3xl">
+<header class="sticky top-0 border-b border-black/10 w-full bg-gray-100/10 backdrop-blur-3xl z-10">
 	<div class="container flex h-12 max-w-screen-2xl items-center justify-between">
 		<a href="/" class="font-medium flex gap-1"
 			><span>
