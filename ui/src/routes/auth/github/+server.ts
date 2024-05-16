470320263b283773bc381b76bad30aebb7399094
Nithin <thenithinraj@gmail.com>
2024-05-16 19:59:24 +0530

    refactor: rename PUBLIC_SERVER_URL to PUBLIC_BASE_URL

--- ui/src/routes/auth/github/+server.ts
+++ ui/src/routes/auth/github/+server.ts
@@ -1,8 +1,8 @@
-import { PUBLIC_SERVER_URL } from '$env/static/public';
+import { PUBLIC_BASE_URL } from '$env/static/public';
 import { redirect } from '@sveltejs/kit';
 
 export async function GET(): Promise<Response> {
 	// TODO: Replace this URL with actual endpoint
-	console.log(PUBLIC_SERVER_URL);
-	return redirect(302, `${PUBLIC_SERVER_URL}/auth/github`);
+	console.log(PUBLIC_BASE_URL);
+	return redirect(302, `${PUBLIC_BASE_URL}/auth/github`);
 }
