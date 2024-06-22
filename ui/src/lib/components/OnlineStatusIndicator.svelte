f092ede1f7185e2511ae363418ec5b7cd1e2ad17
Nithin <thenithinraj@gmail.com>
2024-06-22 11:16:14 +0530

    feat: add websocket online status badge

---
+++ ui/src/lib/components/OnlineStatusIndicator.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Badge } from '$lib/components/ui/badge/index.js';
+	import { Dot, DotFilled } from 'svelte-radix';
+	export let online: boolean;
+</script>
+
+{#if online}
+	<Badge
+		variant="outline"
+		class="bg-green-100 text-green-600 dark:bg-green-900/20 dark:text-green-400 py-1 max-h-6 px-0"
+		><span class="flex place-items-center">
+			<DotFilled />
+			<p class="pr-2">Online</p>
+		</span></Badge
+	>
+{:else}
+	<Badge
+		variant="outline"
+		class="bg-red-100 text-red-600 py-1 max-h-6 dark:bg-red-900/20  dark:text-red-400 px-0"
+	>
+		<span class="flex place-items-center">
+			<Dot />
+			<p class="pr-2">Offline</p>
+		</span>
+	</Badge>
+{/if}
