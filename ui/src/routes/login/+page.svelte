3cf949de6b6b8f5cfcd1ddb1b6df46454ed6c379
Nithin <thenithinraj@gmail.com>
2024-07-02 23:22:50 +0530

    feat: add login page

---
+++ ui/src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Header from '@/components/Header.svelte';
+	import { Button } from '@/components/ui/button';
+</script>
+
+<div class="h-screen bg-gray-50">
+	<Header user={null} />
+
+	<div class="flex flex-col w-full items-center justify-center mt-72">
+		<!-- Box  -->
+		<div
+			class="rounded-md w-1/2 mx-20 border flex flex-col p-6 gap-3 justify-between xl:w-1/5 shadow-lg backdrop-blur-lg bg-gray-50"
+		>
+			<h1 class="text-xl font-medium">Login</h1>
+			<!-- <h2 class="font-normal text-xl tracking-tight text-gray-600">Login</h2> -->
+			<hr />
+			<div class="flex flex-row gap-3 pt-2">
+				<a href="/auth/github" class="w-full">
+					<Button variant="default" class="w-full">Continue with Github</Button>
+				</a>
+				<!-- <Button variant="default" class="w-full">Google</Button> -->
+			</div>
+		</div>
+	</div>
+</div>
