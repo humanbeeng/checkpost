d2a0427a67e441fbecd5c5f21cac5b7ac8e854b0
Nithin <thenithinraj@gmail.com>
2024-07-02 23:20:51 +0530

    feat: add connecting state to ws connection status badge

--- ui/src/lib/components/OnlineStatusIndicator.svelte
+++ ui/src/lib/components/OnlineStatusIndicator.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { Badge } from '$lib/components/ui/badge/index.js';
 	import { Dot, DotFilled } from 'svelte-radix';
-	export let online = false;
+
+	export let state: 'connecting' | 'online' | 'offline' = 'connecting';
 </script>
 
-{#if online}
+{#if state === 'online'}
 	<Badge
 		variant="outline"
 		class="bg-green-100 text-green-600 dark:bg-green-900/20 dark:text-green-400 py-1 max-h-6 px-0"
@@ -13,7 +14,7 @@
 			<p class="pr-2">Listening</p>
 		</span></Badge
 	>
-{:else}
+{:else if state === 'offline'}
 	<Badge
 		variant="outline"
 		class="bg-red-100 text-red-600 py-1 max-h-6 dark:bg-red-900/20  dark:text-red-400 px-0"
@@ -21,6 +22,16 @@
 		<span class="flex place-items-center">
 			<Dot />
 			<p class="pr-2">Offline</p>
+		</span>
+	</Badge>
+{:else if state === 'connecting'}
+	<Badge
+		variant="outline"
+		class="bg-red-100 text-red-600 py-1 max-h-6 dark:bg-red-900/20  dark:text-red-400 px-0"
+	>
+		<span class="flex place-items-center">
+			<Dot />
+			<p class="pr-2">Connecting...</p>
 		</span>
 	</Badge>
 {/if}
