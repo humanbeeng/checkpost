42449d43080d2aedde67066865c8ce746e2d1d5c
Nithin <thenithinraj@gmail.com>
2024-07-04 00:59:50 +0530

    refactor: logs and types

--- ui/src/routes/auth/logout/+page.server.ts
+++ ui/src/routes/auth/logout/+page.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from '../../inspect/[endpoint]/$types';
 
-export const load = async ({ cookies }) => {
+export const load: PageServerLoad = async ({ cookies }) => {
+	console.log("Logout called")
 	cookies.delete('token', { path: '/' });
 	return redirect(302, '/');
 };
